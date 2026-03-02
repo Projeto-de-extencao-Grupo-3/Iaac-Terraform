29f687ace40721a75cacc7c61d412e62effbac73
LucasDev-end <lucas.marcolino@sptech.school>
2026-03-01 21:59:32 -0300

    Editando outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "frontend_1_public_ip" {
-  value = aws_instance.frontend_1.public_ip
+  value = aws_instance.frontend_grotrack.public_ip
 }
 
 output "frontend_2_public_ip" {
-  value = aws_instance.frontend_2.public_ip
+  value = aws_instance.frontend2_grotrack.public_ip
 }
 
 output "backend_private_ip" {
-  value = aws_instance.backend.private_ip
+  value = aws_instance.backend_grotrack.private_ip
 }
 
 output "db_private_ip" {
-  value = aws_instance.db.private_ip
+  value = aws_instance.db_grotrack.private_ip
+}
+
+output "api_private_ip" {
+  value = aws_instance.api_grotrack.private_ip
 }
